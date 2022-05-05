b233d7286cecd08bcc841ecb8979b68e06175b43
Victor <victortondo@gmail.com>
2022-05-05 15:38:48 +0200

    exercise 3

--- 01-base/03-prompt-if/script.js
+++ 01-base/03-prompt-if/script.js
@@ -1,6 +1,6 @@
 /* becode/javascript
  *
- * /01-base/03-prompt-if/script.js - 1.3: promp & if
+ * /01-base/03-prompt-if/script.js - 1.3: prompt & if
  *
  * coded by leny@BeCode
  * started at 26/10/2018
@@ -11,6 +11,12 @@
 
 (function() {
 
-  // your code here
+let theCake = prompt("Do you want more cake?")
 
-})();
+    if (theCake === "yes"){
+       alert("congratulations");
+    } else {
+        alert("more for me then");
+    }
+})
+();
