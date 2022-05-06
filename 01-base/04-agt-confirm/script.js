fab318236601cead1d7c57c5fc18687b989b172e
Victor <victortondo@gmail.com>
2022-05-06 13:25:27 +0200

    exercise 4

--- 01-base/04-agt-confirm/script.js
+++ 01-base/04-agt-confirm/script.js
@@ -11,6 +11,17 @@
 
 (function() {
 
-    // your code here
+    function questions (){
+        let theAge = prompt("How old are you?");
+        let theGender = prompt("What is your gender?");
+        let theTown = prompt("Where do you live?");
+        return " Your age: " + theAge + " Your gender: " + theGender + " Your town or city: " + theTown;
+    }
+    let info = questions ();
+    let confirm = window.confirm("Is this information correct?" + info );
+
+    if (confirm !== true ){
+        questions();
+    }
 
 })();
