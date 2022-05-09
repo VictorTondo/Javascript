6eff1657ed9a90fedec944eeab015e6e2137d94f
Victor <victortondo@gmail.com>
2022-05-09 16:45:27 +0200

    exercise 2

--- 02-colors/02-change-bcg-two/script.js
+++ 02-colors/02-change-bcg-two/script.js
@@ -9,8 +9,18 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+
+
 (function() {
 
-    // your code here
+    const btn = document.getElementById('run').addEventListener('click', function(){
+
+        const color = document.getElementById('color').value;
+
+        document.body.style.backgroundColor=color
+
+        console.log(color)
+
+    });
 
 })();
