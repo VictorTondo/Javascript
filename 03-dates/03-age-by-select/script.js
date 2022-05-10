d548ec489957153196a60c0ea52cc8bc8b932019
Victor <victortondo@gmail.com>
2022-05-10 12:10:15 +0200

    exercise 3

--- 03-dates/03-age-by-select/script.js
+++ 03-dates/03-age-by-select/script.js
@@ -11,6 +11,35 @@
 
 (function() {
 
-    // your code here
+    document.getElementById("run").addEventListener("click", () =>
+    {
+        let d1 = document.getElementById('dob-day').value;
+        let m1 = document.getElementById('dob-month').value;
+        let y1 = document.getElementById('dob-year').value;
+
+        let date = new Date();
+        let d2 = date.getDate();
+        let m2 = date.getMonth() +1;
+        let y2 = date.getFullYear();
+
+        let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+        if(d1 > d2){
+            d2 = d2 + month[m2 - 1];
+            m2 = m2 - 1;
+        }
+        if(m1 > m2){
+            m2 = m2 + 12;
+            y2 = y2 - 1;
+        }
+
+        let d = d2 - d1;
+        let m = m2 - m1;
+        let y = y2 - y1;
+
+
+        window.alert("your age is : " + y + " years and " + m + " months and " + d + " days!");
+    })
 
 })();
+
