7b9480d507bc336804cb9e0b39e0d733b4e56827
Victor <victortondo@gmail.com>
2022-05-10 16:41:37 +0200

    ex2

--- 04-maths/02-calculator-two/script.js
+++ 04-maths/02-calculator-two/script.js
@@ -9,16 +9,34 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-    // to get the value of an input: document.getElementById("element-id").value
 
-    var performOperation = function(operation) {
-        // perform the operation
-    };
+ const performOperation = function (operation) {
 
-    Array.from(document.querySelectorAll("button.operator")).forEach(function($btn) {
-        $btn.addEventListener("click", function() {
-            performOperation($btn.id);
-        });
+    let numberONe = document.getElementById('op-one').value;console.log(numberONe)
+    let numberTwo = document.getElementById('op-two').value;console.log(numberTwo)
+
+        switch (operation) {
+            case 'addition':
+                let sum = parseInt(numberONe) + parseInt(numberTwo);
+                alert("Result is = " + sum);
+                break;
+            case 'subtraction':
+                let rest = parseInt(numberONe) - parseInt(numberTwo);
+                alert("Result is = " + rest);
+                break;
+            case 'multiplication':
+                let mult = parseInt(numberONe) * parseInt(numberTwo);
+                alert("Result is = " + mult);
+                break;
+            case 'division':
+                let div = parseInt(numberONe) / parseInt(numberTwo);
+                alert("Result is = " + div);
+                break;
+
+        }
+ };
+Array.from(document.querySelectorAll("button.operator")).forEach(function ($btn) {
+    $btn.addEventListener("click", function () {
+        performOperation($btn.id);
     });
-})();
+});
