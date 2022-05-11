53f2d51d4caf829f047600360fb5d859f42a191d
Victor <victortondo@gmail.com>
2022-05-11 11:48:12 +0200

    ex 5

--- 05-arrays/05-walk-three/script.js
+++ 05-arrays/05-walk-three/script.js
@@ -11,7 +11,7 @@
 
 (function() {
 
-    var people = [
+    let people = [
         {
             firstname: "Garald",
             lastname: "Freschi",
@@ -39,6 +39,9 @@
         },
     ];
 
-    // your code here
+    document.getElementById("run").addEventListener("click", ()=> {
+    people.forEach(person => {
+        console.log(person.firstname);
+    });
 
-})();
+})})();
