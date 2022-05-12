01961478bc1233c5be78f33a0c38a9430468e989
Victor <victortondo@gmail.com>
2022-05-12 09:05:50 +0200

    exercise 2.1

--- 02-colors/01-change-bcg-one/script.js
+++ 02-colors/01-change-bcg-one/script.js
@@ -9,6 +9,8 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+//
+
 (function() {
 
     const btnRed = document.getElementById('red');
